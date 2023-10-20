efd7164fb8ef0542d7d671ae8a814c1daf9cd19d
gavril0va1707 <gavril0va1707@yandex.ru>
2023-10-20 21:35:05 +0300

    add task08

---
+++ task08/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Напишите программу, которая на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+Console.WriteLine("Введите число: ");
+int NumberA = Convert.ToInt32(Console.ReadLine());
+
+int start = 1;
+
+while (start <= NumberA)
+{
+
+start = start + 1;
+
+if (start % 2 ==0)
+{
+Console.WriteLine(start);
+}
+}
