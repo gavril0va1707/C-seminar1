db4edfcbabc289420640aaf9c6dcd5897e482626
gavril0va1707 <gavril0va1707@yandex.ru>
2023-10-20 08:43:57 +0300

    add task00 and task01

---
+++ task01_два_числа_квадрат/Program.cs
@@ -0,0 +1,25 @@
+﻿/* 
+Напишите программу, которая на вход принимает два числа и проверяет,
+является ли первое число квадратом второго. 
+
+a = 25; b = 5 -> да
+a = 2 b = 10 -> нет
+a = 9; b = -3 -> да
+a = -3 b = 9 -> нет
+*/ 
+
+
+System.Console.WriteLine(" Введите первое число ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine(" Введите второе число ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstNumber == secondNumber * secondNumber) 
+{
+    System.Console.WriteLine("Да, первое число является квадратом второго");
+
+}
+else {
+     System.Console.WriteLine("Нет, первое число не является квадратом второго");
+}
---
+++ tast00_квадрат_числа/Program.cs
@@ -0,0 +1,9 @@
+﻿/*
+     Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя).
+*/
+System.Console.WriteLine(" Введите число "); // cw - быстрый вызов Console.WriteLine
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+int result = userNumber * userNumber;
+System.Console.WriteLine(result);
+
