cc1d25431a481bdbc9209cd5464b3920c3e7f8d3
gavril0va1707 <gavril0va1707@yandex.ru>
2023-10-20 21:58:06 +0300

    add task07

---
+++ task07/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+
+Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+
+456 -> 6
+782 -> 2
+918 -> 8
+*/
+
+using System.Reflection.Metadata;
+
+Console.WriteLine("Введите трехзначное число: ");
+int NumberA = Convert.ToInt32(Console.ReadLine());
+
+if (NumberA >= 100 && NumberA <1000)
+{
+int answer = NumberA % 10;
+
+
+    Console.WriteLine("Последняя цифра числа: " + answer);
+
+}
+else 
+{Console.WriteLine ("Число не является трехначным");
+}
