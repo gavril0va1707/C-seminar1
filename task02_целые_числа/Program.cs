d1d3672c14d0692ebcbc56db7422d05aec775e6f
gavril0va1707 <gavril0va1707@yandex.ru>
2023-10-20 19:48:15 +0300

    add task02

---
+++ task02_целые_числа/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Напишите программу, которая на вход принимает одно число (N), 
+а на выходе показывает все целые числа в промежутке от -N до N. 
+
+4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+2 -> " -2, -1, 0, 1, 2"
+*/
+
+Console.WriteLine("Введите число: ");
+int NumberA = Convert.ToInt32(Console.ReadLine());
+int start = NumberA * -1;
+
+while (start <= NumberA)
+{
+Console.WriteLine(start);
+start = start + 1;
+
+}
