6498497a102ae54df763831650d799f023a42db2
gavril0va1707 <gavril0va1707@yandex.ru>
2023-11-14 20:05:52 +0300

    add task011 исправленный

---
+++ task011/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+
+Напишите программу, которая принимает на вход
+трёхзначное число и на выходе показывает вторую
+цифру этого числа.
+
+*/
+
+Console.WriteLine ("ВВедите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >=100 && number <1000)
+{
+    int first = number / 100;
+int last = number % 10;
+Console.WriteLine("ответ =  " + first+last);
+
+}
+else
+{
+    Console.WriteLine("Это число не трехзначное");
+}
+
+
+
